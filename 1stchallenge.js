2cd2132bcbe4e3ecaaa7a593f3b12d520fd1a042
Debbie Davila <debbie@debbiedavila.com>
2023-10-10 13:32:21 -0400

    notes from class

---
+++ 1stchallenge.js
@@ -0,0 +1,12 @@
+function calculateCelsius(event) {
+    event.preventDefault();
+    const Fahrenheit = event.target.celsius.value;
+    const calculateCelsius = c=(F-32)/1.8
+
+   
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", calculateFahrenheit)
+
+
